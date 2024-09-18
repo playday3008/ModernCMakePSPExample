2978141d316006801ada5034ed1c5a07cebd33d5
PlayDay <18056374+playday3008@users.noreply.github.com>
2024-09-18 14:56:28 +0200

    Refactor testlib.cpp to include necessary headers and add PSP module information

--- tests/testlib.cpp
+++ tests/testlib.cpp
@@ -1,6 +1,11 @@
+#include <modern/lib.hpp>
+
 #include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
-#include <modern/lib.hpp>
+
+#include <pspkernel.h>
+
+PSP_MODULE_INFO("testlib", PSP_MODULE_USER, 1, 0);
 
 TEST_CASE("Quick Check", "[main]") {
     std::vector<double> values{1, 2., 3.};
