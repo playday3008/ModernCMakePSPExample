8b07f7a0c88c573c89fe9597ef2d9a4be15d2cd7
PlayDay <18056374+playday3008@users.noreply.github.com>
2024-09-18 17:49:59 +0200

    chore: Reformat cmake files

--- cmake/ArduinoJson.cmake
+++ cmake/ArduinoJson.cmake
@@ -1,9 +1,11 @@
 include(FetchContent)
+
 FetchContent_Declare(
-    ArduinoJson
-    GIT_REPOSITORY https://github.com/bblanchon/ArduinoJson.git
-    GIT_TAG        v7.1.0
-    GIT_PROGRESS   TRUE
+  ArduinoJson
+  GIT_REPOSITORY https://github.com/bblanchon/ArduinoJson.git
+  GIT_TAG        v7.1.0
+  GIT_PROGRESS   TRUE
+  FIND_PACKAGE_ARGS
 )
 
 message(STATUS "Fetching ArduinoJson")
